d53b897a4b33476564bc02378a5ed7de68848f10
siddhartha15 <siddhartham.171co246@nitk.edu.in>
2019-10-27 16:32:40 +0530

    added

---
+++ models/config.js
@@ -0,0 +1,32 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'nodeuser',
+  password : 'nodeuser@15',
+  database : 'lib_v1'
+});
+
+module.exports = {
+	executeQuery: function(sql, sqlParam, callback){
+		if(sqlParam == null)
+		{
+			connection.query(sql, function(error, result){
+				if(error)
+				{
+					console.log(error);
+				}
+				callback(result);
+			});
+		}
+		else
+		{
+			connection.query(sql, sqlParam, function(error, result){
+				if(error)
+				{
+					console.log(error);
+				}
+				callback(result);
+			});
+		}
+	}
+};
